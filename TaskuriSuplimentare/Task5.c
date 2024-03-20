6ee16989833d14848b088323352f3ee4ab015972
mihai.miercan@gmail.com <mihai.miercan@gmail.com>
2024-03-20 20:32:55 +0200

    Task Suplimentar 5

--- TaskuriSuplimentare/Task5.c
+++ TaskuriSuplimentare/Task5.c
@@ -48,6 +48,69 @@ void citireTXT(const char* numeFisier, struct Nod** nod, int* dim) {
     }
 }
 
+Mamifer* adaugareInVector(Mamifer mamifer, Mamifer* vector, int* dim) {
+    Mamifer* copie = (Mamifer*)malloc(sizeof(Mamifer) * ((*dim) + 1));
+
+    for (int i = 0; i < (*dim); i++) {
+        copie[i] = vector[i];
+    }
+    copie[*dim] = mamifer;
+
+    if (vector != NULL)
+        free(vector);
+
+    (*dim)++;
+    return copie;
+}
+
+void citireTXTSortat(const char* numeFisier, struct Nod** nod, int* dim) {
+    FILE* f = fopen(numeFisier, "r");
+    Mamifer* mamiferVector = NULL;
+
+    if (f != NULL) {
+        char buffer[100];
+        while (fgets(buffer, 99, f) != NULL) {
+            Mamifer mamifer;
+            char delimitator[] = ",\n";
+            char* token = strtok(buffer, delimitator);
+            mamifer.specie = (char*)malloc(sizeof(char) * (strlen(token) + 1));
+            strcpy(mamifer.specie, token);
+
+            token = strtok(NULL, delimitator);
+            mamifer.varstaMaxima = atoi(token);
+
+            token = strtok(NULL, delimitator);
+            mamifer.greutateMedie = atof(token);
+
+            token = strtok(NULL, delimitator);
+            mamifer.nrTotal = atol(token);
+
+            mamiferVector = adaugareInVector(mamifer, mamiferVector, dim);
+        }
+        fclose(f);
+
+        for (int i = 0; i < (*dim) - 1; i++) {
+            for (int j = i + 1; j < (*dim); j++) {
+                if (mamiferVector[i].varstaMaxima < mamiferVector[j].varstaMaxima) {
+                    Mamifer temp = mamiferVector[i];
+                    mamiferVector[i] = mamiferVector[j];
+                    mamiferVector[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < (*dim); i++) {
+            struct Nod* nodNou = (struct Nod*)malloc(sizeof(struct Nod));
+            nodNou->mamifer = mamiferVector[i];
+            nodNou->next = *nod;
+
+            *nod = nodNou;
+        }
+
+        free(mamiferVector);
+    }
+}
+
 void afisareMamifer(Mamifer m) {
     printf("Specie: %s\n", m.specie);
     printf("Varsta: %d\n", m.varstaMaxima);
@@ -81,16 +144,111 @@ void cautareNod(struct Nod* nod, int poz) {
     afisareNod(nod);
 }
 
+Mamifer* inserareInVector_ConditieVarsta(Mamifer* vector, struct Nod* vf, int varsta, int* dim) {
+    struct Nod* vf2 = vf;
+
+    while (vf2 != NULL) {
+        if (vf2->mamifer.varstaMaxima > varsta)
+            (*dim)++;
+
+        vf2 = vf2->next;
+    }
+
+    Mamifer* vectorAux = (Mamifer*)malloc(sizeof(Mamifer) * (*dim));
+
+    int poz = 0;
+    while (vf != NULL) {
+        if (vf->mamifer.varstaMaxima > varsta) {
+            vectorAux[poz] = vf->mamifer;
+            vectorAux[poz].specie = (char*)malloc(sizeof(char) * (strlen(vf->mamifer.specie) + 1));
+            strcpy(vectorAux[poz].specie, vf->mamifer.specie);
+            poz++;
+        }
+
+        vf = vf->next;
+    }
+
+    return vectorAux;
+}
+
+void stergereNodDinLista(struct Nod** nod, int poz) {
+    int i = 0;
+    struct Nod* auxfirst = (*nod);
+    struct Nod* auxlast = (*nod);
+
+    if ((*nod) == NULL) {
+        printf("Lista este deja goala!\n");
+    }
+    else {
+        while (auxfirst->next != NULL && i != poz) {
+            auxlast = auxfirst;
+            auxfirst = auxfirst->next;
+            i++;
+        }
+        if (i == poz) {
+            auxlast->next = auxfirst->next;
+            free(auxfirst->mamifer.specie);
+            free(auxfirst);
+        }
+        else printf("Nu am gasit nodul care trebuia sters!\n");
+    }
+
+}
+
+void dezalocareVector(Mamifer** vector, int* dim) {
+    for (int i = 0; i < (*dim); i++) {
+        free((*vector)[i].specie);
+        (*vector)[i].specie = NULL;
+    }
+    free(*vector);
+    (*vector) = NULL;
+    (*dim) = 0;
+    printf("Vectorul a fost dezalocat cu succes!");
+}
+
+
+void stergeLista(struct Nod** vf) {
+    while ((*vf) != NULL) {
+        free((*vf)->mamifer.specie);
+        struct Nod* copie = *vf;
+        *vf = (*vf)->next;
+
+        free(copie);
+    }
+}
 
 void main() {
+    printf("\n=======================|LISTA 1|=======================\n");
     struct Nod* vf = NULL;
     int dim = 0;
     citireTXT("FisierMamifere.txt", &vf, &dim);
     afisareLista(vf);
     printf("Dimensiune Lista: %d\n", dim);
 
+    printf("\n=======================|LISTA 2|=======================\n");
+    struct Nod* vf2 = NULL;
+    int dim2 = 0;
+    citireTXTSortat("FisierMamifere.txt", &vf2, &dim2);
+    afisareLista(vf2);
+
+    printf("\n============================\n");
     printf("\n");
-    cautareNod(vf, 9);
+    cautareNod(vf, 10);
     printf("\n");
 
+    printf("\n===========VECTOR=============\n");
+    Mamifer* vector = NULL;
+    int dimVector = 0;
+    vector = inserareInVector_ConditieVarsta(vector, vf, 30, &dimVector);
+    for (int i = 0; i < dimVector; i++) {
+        afisareMamifer(vector[i]);
+    }
+
+    printf("\n===========AFISARE DUPA STERGERE NOD X=============\n");
+    stergereNodDinLista(&vf, 3);
+    afisareLista(vf);
+
+    stergeLista(&vf);
+    stergeLista(&vf2);
+    dezalocareVector(&vector, &dimVector);
 }
